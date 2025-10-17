9260ab600d837ce23048b7332b740fa371f54298
Forvater <justmail12345@gmail.com>
2025-10-17 16:19:09 +0200

    no autoindent, no tabs

--- nvim/home/username/.config/nvim/init.lua
+++ nvim/home/username/.config/nvim/init.lua
@@ -1,2 +1,11 @@
 require('config.options')
 require("config.lazy")
+
+vim.cmd([[
+  filetype indent off
+  set noautoindent
+  set nosmartindent
+  set nocindent
+  set indentexpr=
+  set indentkeys=
+]])
