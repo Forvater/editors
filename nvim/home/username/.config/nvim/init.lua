b9139ed3f4541ee2b055f542b885abfdbf56a8d1
Forvater <justmail12345@gmail.com>
2025-10-17 16:43:45 +0200

    lualine relative file path

--- nvim/home/username/.config/nvim/init.lua
+++ nvim/home/username/.config/nvim/init.lua
@@ -9,3 +9,23 @@ vim.cmd([[
   set indentexpr=
   set indentkeys=
 ]])
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {
+      {'filename', file_status = true, path = 1}, -- 1 for relative path
+    },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+  extensions = {'fugitive', 'nvim-tree'}
+}
+
